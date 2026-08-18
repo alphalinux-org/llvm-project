21812619083e6201e932022a87c6012d07f45437
Matt Turner <mattst88@gmail.com>
2026-08-18 03:52:57 -0400

    [Alpha] Do f128 sign operations without a call
    fneg, fabs and copysign only touch the sign bit, which lives in the high
    half of the pair, so they are done inline rather than through OTS.

--- llvm/lib/Target/Alpha/AlphaISelLowering.cpp
+++ llvm/lib/Target/Alpha/AlphaISelLowering.cpp
@@ -288,6 +288,12 @@ SDValue AlphaTargetLowering::PerformDAGCombine(SDNode *N,
     if (SDValue V = LowerF128Compare(N, DCI))
       return V;
     break;
+  case ISD::FNEG:
+  case ISD::FABS:
+  case ISD::FCOPYSIGN:
+    if (SDValue V = LowerF128Bitwise(N, DCI))
+      return V;
+    break;
   default:
     break;
   }
@@ -1319,6 +1325,57 @@ SDValue AlphaTargetLowering::LowerF128Compare(SDNode *N,
     return SDValue(N, 0);
   }
   return Result;
+}
+
+SDValue AlphaTargetLowering::LowerF128Bitwise(SDNode *N,
+                                              DAGCombinerInfo &DCI) const {
+  if (!DCI.isBeforeLegalize())
+    return SDValue();
+  if (N->getSimpleValueType(0) != MVT::f128)
+    return SDValue();
+
+  SelectionDAG &DAG = DCI.DAG;
+  SDLoc DL(N);
+  MachineFunction &MF = DAG.getMachineFunction();
+
+  unsigned Opc = N->getOpcode();
+
+  SDValue Src = N->getOperand(0);
+  auto [Lo, Hi] = splitF128(DAG, DL, MF, DAG.getEntryNode(), Src);
+  SDValue Chain = DAG.getNode(ISD::TokenFactor, DL, MVT::Other,
+                              {Lo.getValue(1), Hi.getValue(1)});
+
+  // The sign bit lives in the most significant bit of the high word.
+  SDValue SignMask = DAG.getConstant(APInt::getSignMask(64), DL, MVT::i64);
+
+  SDValue NewHi;
+  switch (Opc) {
+  case ISD::FNEG:
+    NewHi = DAG.getNode(ISD::XOR, DL, MVT::i64, Hi, SignMask);
+    break;
+  case ISD::FABS:
+    // Clear the sign bit.
+    NewHi = DAG.getNode(ISD::AND, DL, MVT::i64, Hi,
+                        DAG.getNode(ISD::XOR, DL, MVT::i64, SignMask,
+                                    DAG.getConstant(-1ULL, DL, MVT::i64)));
+    break;
+  case ISD::FCOPYSIGN: {
+    // Copy sign from operand 1; the magnitude stays from operand 0.
+    SDValue SignHi =
+        splitF128(DAG, DL, MF, DAG.getEntryNode(), N->getOperand(1)).second;
+    SDValue SignBit = DAG.getNode(ISD::AND, DL, MVT::i64, SignHi, SignMask);
+    SDValue MagBits =
+        DAG.getNode(ISD::AND, DL, MVT::i64, Hi,
+                    DAG.getNode(ISD::XOR, DL, MVT::i64, SignMask,
+                                DAG.getConstant(-1ULL, DL, MVT::i64)));
+    NewHi = DAG.getNode(ISD::OR, DL, MVT::i64, MagBits, SignBit);
+    break;
+  }
+  default:
+    return SDValue();
+  }
+
+  return joinF128(DAG, DL, MF, Chain, Lo, NewHi);
 }
 
 SDValue AlphaTargetLowering::LowerDivRem(SDValue Op, SelectionDAG &DAG) const {
